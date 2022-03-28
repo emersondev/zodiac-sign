5b2866183a977a07896d445749ad3c5df2a116af
Emerson Novais <emersonnovais93@gmail.com>
2022-03-27 23:01:35 -0300

    Initial Commit

---
+++ zodiac-sign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zodiac-sign
+//
+//  Created by Emerson.Novais on 27/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
